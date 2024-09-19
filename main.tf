c866506660829345c338db84bccf34e31ec03996
Roman Grinovski <roman.grinovski@gmail.com>
2024-09-19 23:51:41 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,3 +28,37 @@ module "my_workerpool" {
   spacelift_api_key_id       = var.spacelift_api_key_id
   spacelift_api_key_secret   = var.spacelift_api_key_secret
 }
+
+resource "aws_vpc" "mtc_vpc" {
+  cidr_block           = "10.123.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    Name = "dev2"
+  }
+}
+resource "aws_subnet" "mtc_public_subnet" {
+  vpc_id                  = aws_vpc.mtc_vpc.id
+  cidr_block              = "10.123.1.0/24"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "dev-public"
+  }
+}
+resource "aws_security_group" "mtc_sg" {
+  name        = "public_sg"
+  description = "public security group"
+  vpc_id      = aws_vpc.mtc_vpc.id
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
