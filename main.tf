8f24d7f731fea2b0120337c07748e1662fae0da5
Roman Grinovski <roman.grinovski@gmail.com>
2024-09-19 23:25:29 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,33 +1,30 @@
-resource "aws_vpc" "mtc_vpc" {
-  cidr_block           = "10.123.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-  tags = {
-    Name = "dev2"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.57.0"
+    }
   }
 }
-resource "aws_subnet" "mtc_public_subnet" {
-  vpc_id                  = aws_vpc.mtc_vpc.id
-  cidr_block              = "10.123.1.0/24"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "dev-public"
-  }
+
+provider "aws" {
+  region = "eu-west-1" 
 }
-resource "aws_security_group" "mtc_sg" {
-  name        = "public_sg"
-  description = "public security group"
-  vpc_id      = aws_vpc.mtc_vpc.id
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+
+module "my_workerpool" {
+  source = "github.com/spacelift-io/terraform-aws-spacelift-workerpool-on-ec2?ref=v2.3.1"
+
+  configuration = <<-EOT
+    export SPACELIFT_TOKEN="${var.worker_pool_config}"
+    export SPACELIFT_POOL_PRIVATE_KEY="${var.worker_pool_private_key}"
+  EOT
+
+  min_size          = 1
+  max_size          = 5
+  worker_pool_id    = var.worker_pool_id
+  security_groups   = var.worker_pool_security_groups
+  vpc_subnets       = var.worker_pool_subnets
+  spacelift_api_key_endpoint = var.spacelift_api_key_endpoint
+  spacelift_api_key_id       = var.spacelift_api_key_id
+  spacelift_api_key_secret   = var.spacelift_api_key_secret
 }
